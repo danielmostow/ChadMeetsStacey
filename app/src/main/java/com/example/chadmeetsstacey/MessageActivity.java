4b57165dfe7d8a3747259a80ea7cafdaeaf158d4
jadelstein <adelstein.16@osu.edu>
2019-03-26 10:53:03 -0400

    Created layout screen for message

---
+++ app/src/main/java/com/example/chadmeetsstacey/MessageActivity.java
@@ -0,0 +1,16 @@
+package com.example.chadmeetsstacey;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+    }
+
+}
