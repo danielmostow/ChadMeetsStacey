3b870dfebc00ebba857965a7942b90ff312566ba
jadelstein <adelstein.16@osu.edu>
2019-04-09 15:59:53 -0400

    Added dialog box to confirm delete

--- app/src/main/java/com/example/chadmeetsstacey/EditEventActivity.java
+++ app/src/main/java/com/example/chadmeetsstacey/EditEventActivity.java
@@ -1,5 +1,6 @@
 package com.example.chadmeetsstacey;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.app.AlertDialog;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -138,26 +140,39 @@ public class EditEventActivity extends AppCompatActivity {
 
     // Attempts to delete event
     private void attemptToDeleteEvent() {
-        // TODO: Use dialog box to confirm delete
+        // TODO: Delete matches on delete
+        new AlertDialog.Builder(EditEventActivity.this)
+                .setTitle("Delete event")
+                .setMessage("Are you sure you want to delete this event?")
 
-        // Delete event from database
-        db.collection("events").document(eventID)
-                .delete()
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
-                        finish();
-                        Toast.makeText(getApplicationContext(), eventName.getText().toString() + " deleted!", Toast.LENGTH_LONG).show();
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Delete event from database
+                        db.collection("events").document(eventID)
+                                .delete()
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                                        finish();
+                                        Toast.makeText(getApplicationContext(), eventName.getText().toString() + " deleted!", Toast.LENGTH_LONG).show();
 
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.d(TAG, "Error deleting document", e);
+                                    }
+                                });
                     }
                 })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d(TAG, "Error deleting document", e);
-                    }
-                });
+
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
 
     }
 
